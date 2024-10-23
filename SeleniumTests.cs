ea6f87998c5daeb98c8bc84837fe9d5c29c63487
wknnisa <wknnisa@gmail.com>
2024-10-24 06:59:40 +0800

    Initial commit: add project files without outer folder

---
+++ SeleniumTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework; // Make sure NUnit is installed
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HerokuAppAutomation
+{
+    public class SeleniumTests
+    {
+        private IWebDriver driver;
+
+        [SetUp] // Runs before each test
+        public void Setup()
+        {
+            driver = new ChromeDriver(@"C:\WebDrivers\"); // Update with your path
+        }
+
+        [Test]
+        public void VerifyHomePageTitle()
+        {
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com");
+            //Assert.AreEqual("The Internet", driver.Title);
+        }
+
+        [TearDown] // Runs after each test
+        public void Cleanup()
+        {
+            driver.Quit(); // Closes the browser
+        }
+    }
+}
