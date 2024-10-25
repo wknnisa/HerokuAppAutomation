02522302bf1840df3eb4ee938484584e4d0334cb
wknnisa <wknnisa@gmail.com>
2024-10-25 21:50:48 +0800

    adding 1 new test method

--- SeleniumTests.cs
+++ SeleniumTests.cs
@@ -22,6 +22,28 @@ namespace HerokuAppAutomation
             Assert.That(driver.Title, Is.EqualTo("The Internet"));
         }
 
+        [Test]
+        public void AddRemoveElementsTest()
+        {
+            // Navigate to the Add/Remove Elements page
+            driver!.Navigate().GoToUrl("https://the-internet.herokuapp.com/add_remove_elements/");
+
+            // Click the "Add Element" button
+            var addButton = driver.FindElement(By.XPath("//button[text()='Add Element']")); // Locate the button using XPath
+            addButton.Click(); // Click the button
+
+            // Verify that a new "Delete" button is displayed
+            var deleteButton = driver.FindElement(By.XPath("//button[text()='Delete']"));
+            Assert.That(deleteButton.Displayed, Is.True); // Check if the button is displayed
+
+            // Click the "Delete" button
+            deleteButton.Click();
+
+            // Verify that the "Delete" button is no longer displayed
+            Assert.That(driver.FindElements(By.XPath("//button[text()='Delete']")).Count, Is.EqualTo(0)); // Check count
+        }
+
+
         [TearDown] // Runs after each test
         public void Cleanup()
         {
