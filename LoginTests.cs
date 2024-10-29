2413ec2ba85cc466b953b5a4c698fcbc533907d6
wknnisa <wknnisa@gmail.com>
2024-10-30 07:00:51 +0800

    LoginTests page update: first part

--- LoginTests.cs
+++ LoginTests.cs
@@ -14,6 +14,28 @@ namespace HerokuAppAutomation
             driver = new ChromeDriver(@"C:\WebDrivers\");
         }
 
+        [Test]
+        public void SuccessfulLogin()
+        {
+            driver!.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            // Case Sensitivity Test
+            string username = "tomsmith";
+            string password = "SuperSecretPassword!";
+
+            // Valid Login
+            PerformLogin(username, password);
+        }
+
+        private void PerformLogin(string username, string password) 
+        {
+            driver!.FindElement(By.Id("username")).Clear();
+            driver!.FindElement(By.Id("username")).SendKeys(username);
+            driver!.FindElement(By.Id("password")).Clear();
+            driver!.FindElement(By.Id("password")).SendKeys(password);
+            driver!.FindElement(By.XPath("//button[@type='submit']")).Click();
+        }
+
         [TearDown]
         public void CleanUp() 
         { 
