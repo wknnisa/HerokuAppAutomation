12630d21cb7626a936a801b47964d4552f21103d
wknnisa <wknnisa@gmail.com>
2024-11-15 22:31:36 +0800

    E2E Test: VerifyLoginAndAddRemoveE2ETest second update - Added Page Object Model (POM) for AddRemoveElementsPage - Previous update missed adding the description for the VerifyLoginAndAddRemoveE2ETest.cs test page and the Page Object Model (POM) for LoginPage

---
+++ Pages/AddRemoveElementsPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+
+namespace HerokuAppAutomation.Pages
+{
+    public class AddRemoveElementsPage
+    {
+        private readonly IWebDriver? driver;
+        private const string AddRemoveElementsUrl = "https://the-internet.herokuapp.com/add_remove_elements/";
+
+        // Constructor
+        public AddRemoveElementsPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Locators for Add/Remove Elements
+        private By addButton = By.XPath("//button[text()='Add Element']");
+        private By deleteButtons = By.XPath("//button[text()='Delete']");
+
+        // Method to navigate to Add/Remove Elements page
+        public void NavigateToAddRemoveElements()
+        {
+            driver!.Navigate().GoToUrl(AddRemoveElementsUrl);
+        }
+
+        // Method to add an element
+        public void AddElement()
+        {
+            driver!.FindElement(addButton).Click();
+        }
+
+        // Method to get the count of "Delete" buttons
+        public int GetDeleteButtonCount()
+        {
+            return driver!.FindElements(deleteButtons).Count();
+        }
+
+        // Method to delete an element
+        public void DeleteElement(int index) 
+        { 
+            var deleteButtonsList = driver!.FindElements(deleteButtons);
+            deleteButtonsList[index].Click();
+
+        }
+    }
+}
